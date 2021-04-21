71df49a754c0c4cfb24158d65515c44a2786d120
nata4101 <riki1998911@gmail.com>
2021-04-22 01:07:15 +0900

    UI作業

---
+++ Assets/Takayanagi/UI/WetherUI.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WetherUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class Rotater
+{
+    private float speed;
+    private float rad;
+    private bool is_change;
+
+    public void SetRotate(float speed,float rad)
+    {
+
+    }
+
+    public bool Rotate()
+    {
+
+        return is_change;
+    }
+}
